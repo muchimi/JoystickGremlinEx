5a66e3cfc5d0f30a1457bb634720cbf616ed7da2
muchimi <etienne.martin.emcs@gmail.com>
2024-06-03 14:09:37 -0400

    13.40.13ex (a) - profile folder change
    -- Profile folder changed to Joystick Gremlin Ex to avoid any conflicts with the original Gremlin.
    
    -- If the profile folder does not exist, it will be created, and the contents of the original folder will be copied into it.

--- joystick_gremlin.py
+++ joystick_gremlin.py
@@ -67,7 +67,7 @@ from gremlin.ui.ui_gremlin import Ui_Gremlin
 from gremlin.input_devices import remote_state
 
 APPLICATION_NAME = "Joystick Gremlin Ex"
-APPLICATION_VERSION = "13.40.13ex (a)"
+APPLICATION_VERSION = "13.40.13ex (b)"
 
 
 class GremlinUi(QtWidgets.QMainWindow):
