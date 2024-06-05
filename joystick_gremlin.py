5bbaab0f5765f03d1a65fcbb614e875a896eab8d
muchimi <etienne.martin.emcs@gmail.com>
2024-06-05 18:28:36 -0400

    13.40.13ex (e)
    Version bump for release

--- joystick_gremlin.py
+++ joystick_gremlin.py
@@ -67,7 +67,7 @@ from gremlin.ui.ui_gremlin import Ui_Gremlin
 from gremlin.input_devices import remote_state
 
 APPLICATION_NAME = "Joystick Gremlin Ex"
-APPLICATION_VERSION = "13.40.13ex (d)"
+APPLICATION_VERSION = "13.40.13ex (e)"
 
 
 from gremlin.singleton_decorator import SingletonDecorator
