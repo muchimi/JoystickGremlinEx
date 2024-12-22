75d22c4c4e36ecea11d5630b3e49c3b7f7c20db9
muchimi <etienne.martin.emcs@gmail.com>
2024-12-21 20:18:32 -0500

    restores missing play_sound plugin

--- joystick_gremlin.py
+++ joystick_gremlin.py
@@ -121,7 +121,7 @@ from gremlin.ui.ui_gremlin import Ui_Gremlin
 #from gremlin.input_devices import remote_state
 
 APPLICATION_NAME = "Joystick Gremlin Ex"
-APPLICATION_VERSION = "13.40.16ex (m49a)"
+APPLICATION_VERSION = "13.40.16ex (m49b)"
 
 # the main ui
 ui = None
