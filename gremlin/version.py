31a0b3614f8c0c6a7c94fd9d05f22cee2a6b24c6
muchimi <etienne.martin.emcs@gmail.com>
2026-07-13 12:22:23 -0400

    Update version.py

--- gremlin/version.py
+++ gremlin/version.py
@@ -18,7 +18,7 @@
 from gremlin.singleton_decorator import SingletonDecorator
 
 APPLICATION_NAME = "GremlinEx"
-APPLICATION_BASE = "m77T4B"
+APPLICATION_BASE = "m77T4C"
 APPLICATION_MAIN = "1.0ex"
 APPLICATION_EXE = "gremlinex.exe"
 
